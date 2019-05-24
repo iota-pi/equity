f771e0cce4e6182e5ef521153909507c42e2369b
David Adams <david.adams.16.12@gmail.com>
2019-05-24 11:14:23 +1000

    Pretty list transitions

--- src/components/HistoryDisplay.tsx
+++ src/components/HistoryDisplay.tsx
@@ -5,6 +5,7 @@ import withStyles, { WithStyles } from "@material-ui/core/styles/withStyles";
 import createStyles from "@material-ui/core/styles/createStyles";
 import { Theme } from "@material-ui/core/styles/createMuiTheme";
 import Typography from '@material-ui/core/Typography';
+import FlipMove from 'react-flip-move';
 
 const styles = (theme: Theme) => createStyles({
   root: {
@@ -15,18 +16,17 @@ const styles = (theme: Theme) => createStyles({
     bottom: 0,
   },
   scrollable: {
+    display: 'flex',
+    flexDirection: 'column-reverse',
     overflowY: 'scroll',
     zIndex: 10,
     paddingLeft: 100 + theme.spacing.unit * 2,
     paddingRight: 100 + theme.spacing.unit * 2,
     height: '100%',
-    '&::before,&::after': {
-      display: 'block',
-      content: '""',
-      height: `calc(50% - ${theme.typography.h1.fontSize || '6rem'})`,
-    },
   },
   historyContainer: {
+    display: 'flex',
+    flexDirection: 'column-reverse',
     minHeight: `calc(${theme.typography.h1.fontSize} * 2)`,
     '&::before, &::after': {
       display: 'block',
@@ -34,6 +34,7 @@ const styles = (theme: Theme) => createStyles({
       position: 'absolute',
       left: 0,
       right: 0,
+      zIndex: 100,
       height: theme.spacing.unit * 20,
     },
     '&::before': {
@@ -48,19 +49,31 @@ const styles = (theme: Theme) => createStyles({
   historyRow: {
     textAlign: 'center',
     width: '100%',
-  },
-  larger: {
+    marginBlock: 0,
+    '&>span': {
+      transition: 'all 0.3s ease',
+    },
     [theme.breakpoints.only('xs')]: {
       fontSize: `calc(${theme.typography.h2.fontSize} * 0.85)`,
-      '&:last-child': {
-        fontSize: `calc(${theme.typography.h1.fontSize})`,
+      '&:last-of-type': {
+        '&>span': {
+          color: theme.palette.primary.main,
+          fontSize: `calc(${theme.typography.h1.fontSize})`,
+        },
       },
     },
     [theme.breakpoints.up('sm')]: {
-      '&:last-child': {
-        fontSize: `calc(${theme.typography.h1.fontSize} * 1.15)`,
+      fontSize: `calc(${theme.typography.h2.fontSize})`,
+      '&:last-of-type': {
+        '&>span': {
+          color: theme.palette.primary.main,
+          fontSize: `calc(${theme.typography.h1.fontSize} * 1.15)`,
+        },
       },
     },
+  },
+  verticalSpacer: {
+    height: theme.spacing.unit * 20,
   },
 });
 
@@ -84,39 +97,69 @@ class HistoryDisplay extends Component<Props> {
           style={{marginRight: -this.scrollBarWidth}}
         >
           <div className={classes.historyContainer}>
-            {history.map((numbers, i) => (
-              <Typography
-                variant={i < history.length - 1 ? 'h2' : 'h1'}
-                color={i < history.length - 1 ? 'default' : 'primary'}
-                align="center"
-                className={[classes.historyRow, classes.larger].join(' ')} key={`historyRow-${i}`}
-              >
-                {numbers.map(x => this.playerToString(x)).join(', ')}
-              </Typography>
-            ))}
+            <FlipMove
+              enterAnimation={{
+                from: { opacity: '0', transform: 'translateY(50px)' },
+                to: { opacity: '1', transform: 'translateY(0px)' },
+              }}
+              leaveAnimation={{
+                from: { opacity: '1', transform: 'translateY(0px)' },
+                to: { opacity: '0', transform: 'translateY(50px)' },
+              }}
+              easing="ease-out"
+              duration={300}
+            >
+              <div className={classes.verticalSpacer}></div>
+              {history.map((numbers, i) => (
+                <Typography
+                  key={`historyRow-${i}`}
+                  variant="h1"
+                  align="center"
+                  className={classes.historyRow}
+                >
+                  {this.renderPlayers(numbers)}
+                </Typography>
+              ))}
+              <div className={classes.verticalSpacer}></div>
+            </FlipMove>
           </div>
         </div>
       </div>
     )
   }
 
-  private playerToString = (id: number) => {
-    return this.props.names[id] || (id + 1).toString()
+  private renderPlayers = (players: number[]) => {
+    const toString = (id: number) => {
+      return this.props.names[id] || (id + 1).toString()
+    }
+
+    return (
+      <React.Fragment>
+        {players.map((p, i) => (
+          <React.Fragment key={`player-${i}`}>
+            {i > 0 && <span>, </span>}
+            <span>{toString(p)}</span>
+          </React.Fragment>
+        ))}
+      </React.Fragment>
+    )
   }
 
   componentDidMount () {
     // Get scrollbar width
     const el = ReactDOM.findDOMNode(this.refs.scrollable) as HTMLElement;
-    console.log('componentDidRender()', el.offsetWidth, el.clientWidth);
     this.scrollBarWidth = el.offsetWidth - el.clientWidth;
   }
 
   componentDidUpdate () {
-    const el = ReactDOM.findDOMNode(this.refs.scrollable) as Element;
-    const offset = el.scrollHeight - el.scrollTop - el.clientHeight;
+    this.scrollToBottom();
+  }
+
+  private scrollToBottom = () => {
+    const el = ReactDOM.findDOMNode(this.refs.scrollable) as HTMLElement;
     el.scroll({
       top: el.scrollHeight,
-      behavior: offset > 100 ? 'smooth' : 'auto',
+      behavior: 'smooth',
     });
   }
 }
